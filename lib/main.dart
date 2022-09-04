bdd00607b0827e9c7727d3e537501f3e90983a6f
Michael Hathi <michael@numerative.dev>
2022-09-04 12:16:20 +0530

    Change app bar title

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Crop and Trim Video'),
     );
   }
 }
